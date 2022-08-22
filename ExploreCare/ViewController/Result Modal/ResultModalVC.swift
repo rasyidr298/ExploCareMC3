f1ba12785ee47fbf5c9231196b103109e4e44d29
Marchell <marchellehcram@gmail.com>
2022-08-22 12:20:45 +0700

    fixed launchscreen file issue

--- ExploreCare/ViewController/Result Modal/ResultModalVC.swift
+++ ExploreCare/ViewController/Result Modal/ResultModalVC.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class ResultModalVC: UIViewController {
     
+    // MARK: - IBOutlets and UI Components
     @IBOutlet weak var resultImage: UIImageView!
     @IBOutlet weak var objectNameLabel: UILabel!
     @IBOutlet weak var objectDescriptionLabel: UILabel!
     @IBOutlet weak var voiceOverButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
 
+    // MARK: - Variables
+    
+    // MARK: - Overriden Functions
     init() {
         super.init(nibName: "ResultModalVC", bundle: nil)
         modalPresentationStyle = .overCurrentContext
@@ -30,6 +34,7 @@ class ResultModalVC: UIViewController {
         setupUI()
     }
     
+    // MARK: - UI Setups
     private func setupUI() {
         resultImage.layer.borderColor = UIColor(hex: "#ADC6F6").cgColor
         resultImage.layer.borderWidth = 2
@@ -39,7 +44,10 @@ class ResultModalVC: UIViewController {
         attStr.append(NSAttributedString(string: "split.", attributes: [.font: UIFont.boldSystemFont(ofSize: 15), .foregroundColor: UIColor.black]))
         objectDescriptionLabel.attributedText = attStr
     }
+    
+    // MARK: - Custom Functions
 
+    // MARK: - Actions
     @IBAction func nextAction(_ sender: Any) {
         dismiss(animated: true)
     }
---
+++ ExploreCare/ViewController/Story telling/StorytellingVC.swift
@@ -0,0 +1,17 @@
+//
+//  StorytellingVC.swift
+//  ExploreCare
+//
+//  Created by cleanmac-ada on 22/08/22.
+//
+
+import UIKit
+
+class StorytellingVC: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
